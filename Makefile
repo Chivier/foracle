c7366a45a80f651f4d907558270d2e2f412a99e1
Chivier Humber <chivier.humber@outlook.com>
2021-08-12 11:27:34 +0800

    Greed done

--- Makefile
+++ Makefile
@@ -2,13 +2,13 @@ gen:
 	g++ -O2 test-generate.cpp -o generator
 
 plan1:
-	g++ -O2 intrinsic.cpp -o intrinsic
+	g++ -g intrinsic.cpp -o intrinsic
 
 plan2:
-	g++ -O2 greed.cpp -o greed
+	g++ -g greed.cpp -o greed
 
 plan3:
-	g++ -O2 dp.cpp -o dp
+	g++ -g dp.cpp -o dp
 
 clean:
 	rm -rf generator intrinsic greed dp
--- greed.cpp
+++ greed.cpp
@@ -2,7 +2,7 @@
  * @Author       : Chivier Humber
  * @Date         : 2021-08-11 22:52:25
  * @LastEditors  : Chivier Humber
- * @LastEditTime : 2021-08-11 22:55:49
+ * @LastEditTime : 2021-08-12 11:26:52
  * @Description  : file content
  */
 #include <chrono>
@@ -21,14 +21,24 @@ using namespace std;
 const int kMaxM = 1000;
 const int kMaxN = 1000000;
 
-const double kInterval = 0;
+const int kInterval = 0;
 
 int gN;
 int gM, gm;
+int gFactor;
 std::array<int, kMaxN> gTask;
 std::array<int, kMaxM> gResourcesPlan;
 std::array<int, kMaxN> gPlan; // Resource Amount
-std::array<double, kMaxN> gTime; // Start time
+std::array<int, kMaxN> gTime; // Start time
+
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+int lcm(int a, int b) {
+    return a * b / gcd(a, b);
+}
 
 /**
  * @description: Read Config information
@@ -46,6 +56,10 @@ void ReadConfig() {
     }
     file.close();
     sort(gResourcesPlan.begin(), gResourcesPlan.begin() + gm, std::greater<int>());
+    gFactor = gResourcesPlan[0];
+    for (int index = 1; index < gm; ++index) {
+        gFactor = lcm(gFactor, gResourcesPlan[index]);
+    }
 }
 
 /**
@@ -59,6 +73,8 @@ void ReadTask() {
         file >> gN;
         for (int index = 0; index < gN; ++index) {
             file >> gTask[index];
+            // Promise all the time interval is integer
+            gTask[index] *= gFactor;
         }
     }
     file.close();
@@ -95,6 +111,13 @@ std::vector<int> SplitResource() {
     return split;
 }
 
+void PrintTags(vector<int> const &tags) {
+    for (auto item : tags) {
+        cout << item << " ";
+    }
+    cout << endl;
+}
+
 /**
  * @description: Arrange all the tasks
  * @param {vector<int>} split
@@ -102,54 +125,131 @@ std::vector<int> SplitResource() {
  */
 void Arrange(std::vector<int> split) {
     int split_size = split.size();
-    vector<double> time_list(split_size, 0);
+    vector<int> time_list(split_size, 0);
+    vector<int> arrange_tag(gM, -1);
 
-    double total_time = 0;
+    int total_time = 0;
     // Give a guess on the next task
 
     // First tuen is the same as intrinsic method
+    int start_tag_position = 0;
     for (int index = 0; index < min(split_size, gN); ++index) {
-        time_list[index] = 1.0 * gTask[index] / split[index] + kInterval;
+        time_list[index] = gTask[index] / split[index] + kInterval;
         gTime[index] = 0;
         gPlan[index] = split[index];
 
-        total_time += 1.0 * gTask[index] / split[index];
+        total_time += gTask[index] / split[index];
+        // Place the tag on the corresponding places
+        for (int sub_index = start_tag_position; sub_index < start_tag_position + split[index]; ++sub_index) {
+            arrange_tag[sub_index] = index;
+        }
+
+        start_tag_position += split[index];
     }
 
-    for (int index = min(split_size, gN); index < gN; ++index) {
+    // PrintTags(arrange_tag);
+    int index = min(split_size, gN);
+    while (index < gN) {
+        // If there is already resources available
+        int empty_resources = 0;
+
         // Find the earliest ended task
-        double time = std::numeric_limits<double>::max();
+        int time_select = std::numeric_limits<int>::max();
         int flag_index = -1;
-        for (int sub_index = 0; sub_index < split_size; ++sub_index) {
-            if (time > time_list[sub_index]) {
-                time = time_list[sub_index];
-                flag_index = sub_index;
+        
+        for (int sub_index = 0; sub_index < gM; ++sub_index) {
+            int tag = arrange_tag[sub_index];
+            if (tag == -1) {
+                continue;
+            }
+            int end_time = gTime[tag] + gTask[tag] / gPlan[tag];
+            if (end_time < time_select) {
+                time_select = end_time;
+                flag_index = tag;
             }
         }
         
         // Arrange the index-th task
-        gTime[index] = time_list[flag_index];
-        time_list[flag_index] += 1.0 * gTask[index] / split[flag_index] + kInterval;
-        gPlan[index] = split[flag_index];
-    }
+        empty_resources = 0;
+        for (int sub_index = 0; sub_index < gM; ++sub_index) {
+            int tag = arrange_tag[sub_index];
+            if (tag == -1) {
+                continue;
+            }
+            if (gTime[tag] + gTask[tag] / gPlan[tag] == time_select) {
+                arrange_tag[sub_index] = -1;
+                empty_resources++;
+            }
+        }
 
-    double final_time = 0;
-    double usage_rate = 0;
+        while (empty_resources) {
+            // Use all the empty_resources
+            // Guess the resources for the next task
+            double guess_time = ((double) total_time) / (index - 1);
+            double guess_resource = gTask[index] / guess_time;
+            int resources_next = 0;
+            for (int resource_index = gm - 1; resource_index >= 0; --resource_index) {
+                if (gResourcesPlan[resource_index] <= guess_resource) {
+                    resources_next = gResourcesPlan[resource_index];
+                }
+            }
+            // Promise the usage of resources is legal
+            resources_next = min(resources_next, empty_resources);
+            empty_resources -= resources_next;
+            gTime[index] = time_select;
+            gPlan[index] = resources_next;
 
-    for (int index = 0; index < split_size; ++index) {
-        if (time_list[index] > final_time) {
-            final_time = time_list[index];
+            int tag_count = 0;
+            for (int sub_index = 0; sub_index < gM; ++sub_index) {
+                if (arrange_tag[sub_index] == -1) {
+                    arrange_tag[sub_index] = index;
+                    tag_count++;
+                }
+            }
+            index++;
+
+            if (index >= gN) {
+                break;
+            }
         }
     }
 
-    for (int index = 0; index < split_size; ++index) {
-        usage_rate += split[index] * (final_time - time_list[index]);
+    int final_time = 0;
+    int begin_time = std::numeric_limits<int>::max();
+    double usage_rate = 0;
+
+    for (int index = 0; index < gM; ++index) {
+        int tag = arrange_tag[index];
+        if (tag == -1) {
+            continue;
+        }
+        int end_time = gTime[tag] + gTask[tag] / gPlan[tag];
+        if (end_time > final_time) {
+            final_time = end_time;
+        }
+        if (end_time < begin_time) {
+            begin_time = end_time;
+        }
     }
+
+    for (int index = 0; index < gM; ++index) {
+        int tag = arrange_tag[index];
+        if (tag == -1) {
+            usage_rate += (final_time - begin_time);
+        }
+        int end_time = gTime[tag] + gTask[tag] / gPlan[tag];
+        usage_rate += (final_time - end_time);
+    }
+
 
     usage_rate = 1 - usage_rate / (gM * final_time);
     usage_rate *= 100;
     
-    cout << "Final time = " << final_time << endl;
+    cout << "Plan :" << endl;
+    for (int index = 0; index < gN; ++index) {
+        cout << index << " begin time = "<< (double)gTime[index] / gFactor << "   resources = " << gPlan[index] << endl;
+    }
+    cout << "Final time = " << ((double) final_time) / gFactor << endl;
     cout << "Usage rate = " << usage_rate << " %" << endl;
 }
 
