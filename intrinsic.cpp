21314656f0eaacffd329fb9ff66e766139e6cb99
Chivier Humber <chivier.humber@outlook.com>
2021-08-15 11:49:13 +0800

    int mode intrinsic is done

--- intrinsic.cpp
+++ intrinsic.cpp
@@ -2,7 +2,7 @@
  * @Author       : Chivier Humber
  * @Date         : 2021-08-11 21:54:56
  * @LastEditors  : Chivier Humber
- * @LastEditTime : 2021-08-11 22:52:13
+ * @LastEditTime : 2021-08-15 11:48:32
  * @Description  : An intrinsic idea
  */
 
@@ -22,14 +22,14 @@ using namespace std;
 const int kMaxM = 1000;
 const int kMaxN = 1000000;
 
-const double kInterval = 0;
+const int kInterval = 0;
 
 int gN;
 int gM, gm;
 std::array<int, kMaxN> gTask;
 std::array<int, kMaxM> gResourcesPlan;
 std::array<int, kMaxN> gPlan; // Resource Amount
-std::array<double, kMaxN> gTime; // Start time
+std::array<int, kMaxN> gTime; // Start time
 
 /**
  * @description: Read Config information
@@ -96,6 +96,10 @@ std::vector<int> SplitResource() {
     return split;
 }
 
+inline int TimeEstimate(int u, int v) {
+    return (u - 1) / v + 1;
+}
+
 /**
  * @description: Arrange all the tasks
  * @param {vector<int>} split
@@ -103,10 +107,13 @@ std::vector<int> SplitResource() {
  */
 void Arrange(std::vector<int> split) {
     int split_size = split.size();
-    vector<double> time_list(split_size, 0);
+    vector<int> time_list(split_size, 0);
+    double final_time = 0;
+    double usage_rate = 0;
 
     for (int index = 0; index < min(split_size, gN); ++index) {
-        time_list[index] = 1.0 * gTask[index] / split[index] + kInterval;
+        time_list[index] = TimeEstimate(gTask[index], split[index]) + kInterval;
+        usage_rate += TimeEstimate(gTask[index], split[index]) * split[index] - gTime[index];
         gTime[index] = 0;
         gPlan[index] = split[index];
     }
@@ -124,12 +131,11 @@ void Arrange(std::vector<int> split) {
         
         // Arrange the index-th task
         gTime[index] = time_list[flag_index];
-        time_list[flag_index] += 1.0 * gTask[index] / split[flag_index] + kInterval;
+        time_list[flag_index] += TimeEstimate(gTask[index], split[flag_index]) + kInterval;
+        usage_rate += TimeEstimate(gTask[index], split[flag_index]) * split[flag_index] - gTime[index];
         gPlan[index] = split[flag_index];
     }
 
-    double final_time = 0;
-    double usage_rate = 0;
 
     for (int index = 0; index < split_size; ++index) {
         if (time_list[index] > final_time) {
@@ -141,6 +147,11 @@ void Arrange(std::vector<int> split) {
         usage_rate += split[index] * (final_time - time_list[index]);
     }
 
+        cout << "Plan :" << endl;
+    for (int index = 0; index < gN; ++index) {
+        cout << index << " task = "
+        << gTask[index] << " begin time = "<< gTime[index] << "   resources = " << gPlan[index] << endl;
+    }
     usage_rate = 1 - usage_rate / (gM * final_time);
     usage_rate *= 100;
     
